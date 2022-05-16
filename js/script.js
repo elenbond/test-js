658dac0c8c095ecba3b2472a292a1f5d6809cf67
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2022-05-16 23:42:42 +0300

    module5

--- js/script.js
+++ js/script.js
@@ -1310,3 +1310,247 @@
 // console.log(getTotalBalanceByGender(users, "female"));
 // console.log(getTotalBalanceByGender(users, "male"));
 
+// module 5
+// #1
+// const pizzaPalace = {
+//   pizzas: ["Supercheese", "Smoked", "Four meats"],
+//   // Change code below this line
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+//   // Change code above this line
+// };
+// console.log(pizzaPalace.order("Smoked"));
+
+// #2
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// #10
+// class Storage {
+//     constructor(items) {
+//         this.items = items;
+//     };
+//     getItems() {
+//         return this.items;
+//     };
+//     addItem(newItem) {
+//         return this.items.push(newItem);
+//     };
+//     removeItem(itemToRemove) {
+//         return this.items = this.items.filter(item => item != itemToRemove);
+//     };
+// }
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// #11
+// class StringBuilder{
+//     constructor(initialValue){
+//         this.value = initialValue;
+//     };
+//     getValue() {
+//         return this.value;
+//     };
+//     padEnd(str) {
+//         this.value = this.value + str;
+//     };
+//     padStart(str) {
+//         this.value = str + this.value;
+//     };
+//     padBoth(str) {
+//         this.value = str + this.value + str;
+//     };
+// }
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// #13
+// class Storage {
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//     };
+
+//   getItems() {
+//     return this.#items;
+//     };
+
+//   addItem(newItem) {
+//    this.#items.push(newItem);
+//     };
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//     };
+// };
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+// #14
+// class Car {
+//     static MAX_PRICE = "50000";
+//   #price;
+//   constructor(price) {
+//     this.#price = price;
+//   }
+//   get price() {
+//     return this.#price;
+//   }
+//   set price(newPrice) {
+//       if (newPrice <= Car.MAX_PRICE) {
+//           this.#price = newPrice;
+//       };
+//       return this.#price;
+//   }
+// }
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// #17
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   static checkPrice(price) {
+//     if(price >= Car.#MAX_PRICE){
+//       return "Error! Price exceeds the maximum";
+//     };
+//     return "Success! Price is within acceptable limits";
+//   }
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// #19
+// class User {
+//     email;
+//     constructor(email) {
+//         this.email = email;
+//     }
+//     get email() {
+//         return this.email;
+//     }
+//     set email(newEmail) {
+//         this.email = newEmail;
+//     }
+// };
+// class Admin extends User {
+//     static AccessLevel = {
+//         BASIC: "basic",
+//         SUPERUSER: "superuser",
+//     }
+//     accessLevel;
+//     consrtuctor({ email, accessLevel }) {
+//         super(email);
+//         this.accessLevel = accessLevel;
+//     }
+// };
+// const mango = new Admin({
+//     email: "mango@mail.com",
+//     accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// #20
+// class User {
+//   email;
+//   constructor(email) {
+//     this.email = email;
+//   }
+//   get email() {
+//     return this.email;
+//   }
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+//     blacklistedEmails = [];
+//     constructor({ email, accessLevel}) {
+//         super(email);
+//         this.accessLevel = accessLevel;
+//     }
+//   blacklist(email) {
+//     this.blacklistedEmails.push(email);
+//   }
+//   isBlacklisted(email) {
+//     if(this.blacklistedEmails.includes(email)) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
